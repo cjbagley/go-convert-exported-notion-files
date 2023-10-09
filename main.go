9450824d1fa6d4e182e83dfe7d19f1947d28210d
Colin Bagley <cjbagley@googlemail.com>
2023-10-09 13:15:12 +0100

    WIP

--- main.go
+++ main.go
@@ -7,14 +7,31 @@ import (
 )
 
 func main() {
-    readFile := flag.Arg(0)
-    if readFile == "" {
+    flag.Parse()
+    dir := flag.Arg(0)
+    
+    if dir == "" {
         exit("Please specify a directory")
-
     }
+
+    isDir, err := isValidDirectory(dir)
+    if !isDir || err != nil {
+        exit("Not a directory")
+    }
+
+    fmt.Println("okay")
 }
 
 func exit(message string) {
     fmt.Println(message)
     os.Exit(1)
+}
+
+func isValidDirectory(path string) (bool, error) {
+    info, err := os.Stat(path)
+    if err != nil {
+        return false, err
+    }
+
+    return info.IsDir(), nil
 }
